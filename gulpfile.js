bb48bf793ca8b3f8d64ce6a8753e0a7de1d95ec9
Yefim Vedernikoff <yefim323@gmail.com>
2017-09-05 20:35:41 -0700

    Add connect config

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 const gulp = require('gulp');
+const connect = require('gulp-connect');
 const nunjucks = require('gulp-nunjucks');
 
-gulp.task('default', () => {
+gulp.task('default', ['serve', 'watch']);
+
+gulp.task('serve', () => {
+  return connect.server({
+    root: 'dist',
+    livereload: true,
+    port: 7890
+  });
+});
+
+gulp.task('watch', ['templates:watch']);
+
+gulp.task('templates:watch', () => {
+  return gulp.watch('templates/*.html', ['templates'])
+});
+
+gulp.task('templates', () => {
   return gulp.src('templates/index.html')
     .pipe(nunjucks.compile())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
 });
